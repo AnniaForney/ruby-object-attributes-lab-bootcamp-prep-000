8f49df57da5d662a05349a5ba658b4227780a339
Annia Forney <anniaforney@gmail.com>
2019-04-07 14:17:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog 
-  def name = (name)
+  
+  def name =(name)
     @name = name 
   end
   
@@ -7,7 +8,7 @@ class Dog
     @name 
   end 
   
-  def breed = (breed)
+  def breed =(breed)
     @breed = breed
   end 
 end 
